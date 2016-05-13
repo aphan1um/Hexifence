5deee5c84382229ccf9e2719d88c3b30396f2c16
candy_dandy <aphan1@student.unimelb.edu.au>
2016-05-13 18:24:50 +1000

    Change Board.java to be more similar to proj 1

--- BookLearning/src/core/Main.java
+++ BookLearning/src/core/Main.java
@@ -2,37 +2,32 @@ package core;
 
 import java.util.Stack;
 
+import aiproj.hexifence.Piece;
+
 public class Main {
 	private static final int DIM = 1;
+	public static final Piece myColor = Piece.RED;
 	
 	public static void main(String[] args) {
 		Board b = new Board(DIM);
 		
-		// add sample points (to test for rotation)
-		b.occupyEdge(1, 0, true);
-		b.occupyEdge(2, 0, true);
-		b.occupyEdge(2, 2, true);
-		b.occupyEdge(4, 1, true);
-		System.out.println(b.toString());
-		System.out.println(b.rotateBoard().toString());
-		
 		System.out.println("Performing DFS...");
-		// depth_search();
-		System.out.println("Minimax value of initial state: " + minimax_value(new SearchTree.Node(new Board(DIM), null, false)));
+		System.out.println("Minimax value of initial state: " + 
+				minimax_value(new SearchTree.Node(new Board(DIM), null, myColor)));
 	}
-	
+
 	public static int minimax_value(SearchTree.Node n) {
 		if (n.getState().isFinished()) {
 			// ystem.out.println(n.getState().getScore());
 			return n.getState().getScore();
 		}
-
+		
 		expand_node(n);
 		// assume a child will always be made (this is checked from above)
 		// TODO: fix duplicity
 		int value = minimax_value(n.getChildren().get(0));
 
-		if (n.isMyTurn()) {		// my turn => maximise score
+		if (n.getColorTurn() == myColor) {		// my turn => maximise score
 			for (SearchTree.Node child : n.getChildren()) {
 				value = Math.max(value, minimax_value(child));
 			}
@@ -47,26 +42,20 @@ public class Main {
 		return value;
 	}
 	
-	/** A iterative version of depth first search.
-	 */
 	public static void expand_node(SearchTree.Node n) {
-		// if the board is full; terminal state
 		if (n.getState().isFinished()) {
-			// do nothing
-		} else {
-			Board child_state = n.getState().deepCopy();
+			return;
+		}
+		
+		Board child_state = n.getState().deepCopy();
 
-			for (int r = 0; r < 4 * DIM - 1; r++) {
-				int column_in_r = 2 * DIM + r - 2 * Math.max(0, r - (2 * DIM - 1));
-
-				for (int c = 0; c < column_in_r; c++) {
-					if (child_state.occupyEdge(r, c + Math.max(0, r - (2 * DIM - 1)), n.isMyTurn()) == 0) {
-						n.addChild(child_state);
-						// System.out.println("Added: " + child_state);
-					}
-
-					child_state = n.getState().deepCopy();
+		for (int r = 0; r < 4 * DIM - 1; r++) {
+			for (int c = 0; c < child_state.getEdges()[r].length; c++) {
+				if (child_state.occupyEdge(r, c + Math.max(0, r - (2 * DIM - 1)), n.getColorTurn())) {
+					n.addChild(child_state);
 				}
+
+				child_state = n.getState().deepCopy();
 			}
 		}
 	}
--- BookLearning/src/core/SearchTree.java
+++ BookLearning/src/core/SearchTree.java
@@ -3,11 +3,15 @@ package core;
 import java.util.ArrayList;
 import java.util.List;
 
+import aiproj.hexifence.Piece;
+
 public class SearchTree {
     private Node root;
 
-    public SearchTree(Board initState, boolean isMyTurn) {
-        root = new Node(initState, null, isMyTurn);
+    public SearchTree(Board initState, Piece colorTurn) {
+    	// ensure the player is either BLUE or RED from Piece enum
+    	assert(colorTurn == Piece.BLUE || colorTurn == Piece.RED);
+        root = new Node(initState, null, colorTurn);
     }
     
     public Node getRoot() {
@@ -18,14 +22,14 @@ public class SearchTree {
         private Board state;
         private Node parent;
         private List<Node> children;
-        private boolean myTurn;
+        private Piece colorTurn;
         
         private int minimax;
         
-        public Node(Board board, Node parent, boolean isMyTurn) {
+        public Node(Board board, Node parent, Piece colorTurn) {
         	this.state = board;
         	this.parent = parent;
-        	this.myTurn = isMyTurn;
+        	this.colorTurn = colorTurn;
         	
         	this.children = new ArrayList<Node>();
         }
@@ -42,8 +46,8 @@ public class SearchTree {
         	return children;
         }
         
-        public boolean isMyTurn() {
-        	return myTurn;
+        public Piece getColorTurn() {
+        	return colorTurn;
         }
         
         public void setMiniMax(int value) {
@@ -51,7 +55,8 @@ public class SearchTree {
         }
         
         public Node addChild(Board board) {
-        	Node new_node = new Node(board, this, !myTurn);
+        	Node new_node = new Node(board, this,
+        			(colorTurn == Piece.BLUE) ? Piece.RED : Piece.BLUE);
         	
         	children.add(new_node);
         	return new_node;
