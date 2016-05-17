8d88533e07c20ab738cbf498eda7bf385b213b99
marty142857 <martycheong@gmail.com>
2016-05-18 00:22:26 +1000

    Weight update.

--- BookLearning/src/aiproj/hexifence/MartinAndy/LearningTest.java
+++ BookLearning/src/aiproj/hexifence/MartinAndy/LearningTest.java
@@ -11,21 +11,22 @@ public class LearningTest {
 	private static final int THREE_DIM = 3;
 	private static final int SEED = 54321;
 	
-	private static final int NUM_SAMPLES = 10000;
-	private static final double LEARNING_RATE = 0.01;
+	private static final int NUM_SAMPLES = 100;
+	private static final double LEARNING_RATE = 0.1;
 	
 	public static void main(String[] args) {
 		Random rand = new Random(SEED);
 		
-		for (int i = 0; i < NUM_SAMPLES; i++) {
-			int cellFeature = 0;
-			
+		double cellWeight = 0, chainWeight = 0;
+		
+		for (int i = 0; i < NUM_SAMPLES; i++) {			
 			int dim = rand.nextBoolean() ? TWO_DIM : THREE_DIM;
 			
 			Board board = new Board(dim, rand.nextBoolean() ? Piece.BLUE : Piece.RED);
 			
 			GradientDescentLearn utilityCalc = new GradientDescentLearn(dim);
 			
+			// Randomly generate a board for each training sample.
 			for (int j = 0; j < board.getEdges().length; j++) {
 				int offset = Math.max(0, j - (2*dim - 1));
 				
@@ -34,25 +35,49 @@ public class LearningTest {
 						continue;
 					}
 					
-					board.occupyEdge(j, k + offset, rand.nextBoolean() ? Piece.BLUE : Piece.RED);
+					int randInt = rand.nextInt(3);
+					
+					int piece;
+					if (randInt == 0) {
+						piece = Piece.BLUE;
+					} else if (randInt == 1) {
+						piece = Piece.RED;
+					} else {
+						piece = Piece.EMPTY;
+					}
+					
+					board.occupyEdge(j, k + offset, piece);
 				}
 			}
 			
+			// Get the number of cells with 5 edges filled in.
 			int[][] edges = board.getEdges();
 			
 			List<Point> cells = board.getUncapturedCells();
 			
+			int cellFeature = 0;
 			for (Point cell : cells) {
 				if (board.getNumOpen(cell.x, cell.y) == 1) {
 					cellFeature++;
 				}
 			}
 			
-			int[] ret = utilityCalc.minimax_value(board);
-			int minimax = ret[0];
+			// Get the number of capturable chains on the board.
+			List<Chain> chains = ConnectedGraph.detectSCC(board).getChains();
+			int chainFeature = chains.size();
 			
-			System.out.println("Minimax: " + minimax + " Cell Feature: " + cellFeature);
-		}
+			// Get the score margin (the utility value).
+			int[] ret = utilityCalc.minimax_value(board);
+			int scoreMargin = ret[0];
+			
+			System.out.println("Dim " + board.getDim() + " Cell Feature " + cellFeature + " Chain Feature " + chainFeature + " Utility: " + scoreMargin);
+			
+			// Weight update.			
+			cellWeight = cellWeight - LEARNING_RATE*(-1*cellFeature - chainFeature - scoreMargin)*cellFeature;
+			chainWeight = chainWeight - LEARNING_RATE*(-1*cellFeature - chainFeature - scoreMargin)*chainFeature;
+			System.out.println("Cell feature weight: " + cellWeight);
+			System.out.println("Chain feature weight: " + chainWeight);
+		}		
 	}
 
 }
